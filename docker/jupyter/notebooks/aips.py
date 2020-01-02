9afeb0fecdbd7c988114c170d3ec1b7186dfbd77
Trey Grainger <code@treygrainger.com>
2020-01-01 22:39:02 -0500

    ch3 fixes

--- docker/jupyter/notebooks/aips.py
+++ docker/jupyter/notebooks/aips.py
@@ -1,9 +1,23 @@
 import requests
+import os
+
+#AIPS_SOLR_HOST = "aips-solr"
+#AIPS_NOTEBOOK_HOST="aips-notebook"
+#AIPS_ZK_HOST="aips-zk"
+#AIPS_SOLR_HOST = "localhost"
+#AIPS_NOTEBOOK_HOST="localhost"
+#AIPS_ZK_HOST="localhost"
+AIPS_SOLR_PORT = "8983"
+AIPS_NOTEBOOK_PORT="8888"
+AIPS_ZK_PORT="2181"
+
+solr_url = 'http://' + AIPS_SOLR_HOST + ':' + AIPS_SOLR_PORT + '/solr/'
+solr_collections_api = solr_url + 'admin/collections'  
 
 def healthcheck():
   import requests    
     
-  status_url = 'http://localhost:8983/solr/admin/zookeeper/statu'
+  status_url = solr_url + 'admin/zookeeper/status'
 
   try:
     response = requests.get(status_url).json()
@@ -16,9 +30,6 @@ def healthcheck():
 
 def print_status(solr_response):
   print("Status: Success" if solr_response["responseHeader"]["status"] == 0 else "Status: Failure; Response:[ " + str(solr_response) + " ]" )    
-
-solr_url = 'http://localhost:8983/solr/'
-solr_collections_api = solr_url + 'admin/collections'
 
 def create_collection(collection_name):
     #Wipe previous collection
@@ -60,3 +71,74 @@ def vec2str(vector):
 
 def tokenize(text):
   return text.replace(".","").replace(",","").lower().split()
+
+"""
+class environment:
+  
+  self._AIPS_SOLR_HOST = "aips-solr"
+  self._AIPS_NOTEBOOK_HOST="aips-notebook"
+  self._AIPS_ZK_HOST="aips-zk"
+  self._AIPS_SOLR_PORT = "8983"
+  self._AIPS_NOTEBOOK_PORT="8888"
+  self._AIPS_ZK_PORT="2181"
+  self._AIPS_SOLR_URL=''
+  self._AIPS_SOLR_COLLECTIONS_API=''
+
+  if "AIPS_HOST" in os.environ: 
+    self._AIPS_SOLR_HOST=os.environ['AIPS_HOST']
+    self._AIPS_NOTEBOOK_HOST=os.environ['AIPS_HOST']  
+    self._AIPS_ZK_HOST=os.environ['AIPS_HOST']
+
+  if "AIPS_SOLR_HOST" in os.environ: 
+    self._AIPS_SOLR_HOST=os.environ['AIPS_SOLR_HOST']
+
+  if "AIPS_NOTEBOOK_HOST" in os.environ:   
+    self._AIPS_NOTEBOOK_HOST=os.environ['AIPS_NOTEBOOK_HOST']
+
+  if "AIPS_ZK_HOST" in os.environ:    
+    self._AIPS_ZK_HOST=os.environ['AIPS_ZK_HOST']
+
+  if "AIPS_SOLR_PORT" in os.environ: 
+    self._AIPS_SOLR_PORT=os.environ['AIPS_SOLR_PORT']
+  
+  if "AIPS_NOTEBOOK_HOST" in os.environ: 
+    self._AIPS_NOTEBOOK_PORT=os.environ['AIPS_NOTEBOOK_PORT']
+
+  if "AIPS_ZK_HOST" in os.environ: 
+    self._AIPS_ZK_PORT=os.environ['AIPS_ZK_PORT']  
+
+  self._AIPS_SOLR_URL = 'http://' + AIPS_SOLR_HOST + ':' + AIPS_SOLR_PORT + '/solr/'
+  self._AIPS_SOLR_COLLECTIONS_API = AIPS_SOLR_URL + 'admin/collections'  
+
+  @property
+  def SOLR_HOST(self):
+    return self._AIPS_SOLR_HOST
+
+  @property
+  def NOTEBOOK_HOST(self):
+    return self._AIPS_SOLR_HOST
+
+  @property
+  def ZK_HOST(self):
+    return self._AIPS_ZK_HOST
+
+  @property
+  def SOLR_PORT(self):
+    return self._AIPS_SOLR_PORT
+
+  @property
+  def NOTEBOOK_PORT(self):
+    return self._AIPS_NOTEBOOK_PORT
+
+  @property
+  def ZK_PORT(self):
+    return self._AIPS_ZK_PORT
+
+  @property
+  def SOLR_URL(self):
+    return self._AIPS_SOLR_URL
+
+  @property
+  def SOLR_COLLECTIONS_API(self):
+    return self._AIPS_SOLR_COLLECTIONS_API
+"""
