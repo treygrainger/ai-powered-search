5208ca00d252debdfc9be423a1d082e6a1f50852
Trey Grainger <code@treygrainger.com>
2024-01-14 20:28:41 -0500

    Finish ch7

--- docker/data-science/notebooks/solr.py
+++ docker/data-science/notebooks/solr.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 AIPS_SOLR_HOST = "aips-solr"
-AIPS_SOLR_HOST = "localhost"
+#AIPS_SOLR_HOST = "localhost"
 AIPS_SOLR_PORT = os.getenv('AIPS_SOLR_PORT') or '8983'
 
 SOLR_URL = f'http://{AIPS_SOLR_HOST}:{AIPS_SOLR_PORT}/solr'
--- docker/data-science/notebooks/webserver/semantic_search/semantic_functions/location_distance.py
+++ docker/data-science/notebooks/webserver/semantic_search/semantic_functions/location_distance.py
@@ -3,64 +3,17 @@ sys.path.append('..')
 import json
 from semantic_search.engine.tag_places import *
 
-def location_distance(query, position):
-      
-    #TODO: Notes. Need a "multi-location-hopping" resolver in here. For example,
-    #hotels near bbq near haystack. This is a search for doctype=hotels, join with (doctype=restaurant AND bbq OR barbecue OR ...) filtered on distance. The first "near" requires pushing the second to a sub-query (join/graph) and then takes over as the actual location distance command.
-      
+def location_distance(query, position):      
     if (len(query['query_tree']) -1 > position):
         nextEntity = query['query_tree'][position + 1]
         if (nextEntity['type'] == "city"):
         
-            query['query_tree'].pop(position + 1); #remove next element since we're inserting into command's position
+            query['query_tree'].pop(position + 1);
             query['query_tree'][position] = {"type":"solr", 
                                              "query": create_geo_filter(nextEntity['location_p'], 
                                              "location_p", 50)}
             return True
-         
-        elif 'coordinates_pt' in nextEntity:
-            query['query_tree'].pop(position + 1) #remove next element since we're inserting into command's position
-            query['query_tree'][position] = {"type":"solr", 
-                                             "query": create_geo_filter(nextEntity['coordinates_pt'], 
-                                             "coordinates_pt", 50) }
-            return True
-        elif nextEntity['type'] == "event": 
-            #nextEntity doesn't have coordinates on it, so try traversing to find coordinates on a parent node (i.e. a venue)
-            query['query_tree'].pop(position + 1); #remove next element since we're inserting into command's position
-           
-            quickHack = None
-
-            if (nextEntity['canonical_form'] == "activate conference"):
-                quickHack = "activate"
-           
-            if (nextEntity['canonical_form'] == "haystack conference"):
-                quickHack = "haystack"
-           
-            attemptedGraphLookup = find_location_coordinates(quickHack)
-
-            if ('response' in attemptedGraphLookup 
-               and 'docs' in attemptedGraphLookup['response']
-               and len(attemptedGraphLookup['response']['docs']) > 0):
-                query['query_tree'][position] = \
-                    { "type":"solr", 
-                      "query": create_geo_filter(
-                          attemptedGraphLookup['response']['docs'][0]['coordinates_s'], 
-                          "coordinates_pt", 50)
-                    }
-                return True
-
     return False
 
 def create_geo_filter(coordinates, field, distanceInKM):
     return "+{!geofilt d=" + str(distanceInKM) + " sfield=\"" + field + "\" pt=\"" + coordinates + "\"}"
-
-def find_location_coordinates(keyword):      
-    query =  {
-        "params": {
-        "qf": "name_t",
-        "keywords": keyword
-        },
-        "query": "{!graph from=name_s to=venue_s returnOnlyLeaf=true}{!edismax v=$keywords}"
-    }        
-    
-    return json.loads(tag_places(query))
