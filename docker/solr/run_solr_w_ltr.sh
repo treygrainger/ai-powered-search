5c07bab9c8e975fd3ce307f97d2cade7bd2ee474
Daniel Crouch <dcrouch26@gmail.com>
2024-01-20 22:35:27 -0800

    Solr 9 upgraded, chapter 15 has an issue (functions but can't find prompts

--- docker/solr/run_solr_w_ltr.sh
+++ docker/solr/run_solr_w_ltr.sh
@@ -3,4 +3,5 @@ mkdir -p /var/solr/data/lib/
 cp dist/solr-ltr-*.jar /var/solr/data/lib/
 ls /var/solr/data/lib
 
-solr-foreground -Dsolr.ltr.enabled=true
+SOLR_MODULES=ltr
+solr-foreground -Dsolr.ltr.enabled=true 
