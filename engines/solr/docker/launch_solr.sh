17bb7ec474de2157cbeac4df1f2aff9fe2de8ed5
Daniel Crouch <dcrouch26@users.noreply.github.com>
2024-05-25 23:58:40 -0400

    deletes solr/docker

--- engines/solr/docker/launch_solr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build . -t aips-solr
-docker-compose up
--- engines/solr/docker/run_solr_w_ltr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-mkdir -p /var/solr/data/lib/
-cp dist/solr-ltr-*.jar /var/solr/data/lib/
-ls /var/solr/data/lib
-
-SOLR_MODULES=ltr
-solr-foreground -Dsolr.modules=ltr -Dsolr.ltr.enabled=true 
