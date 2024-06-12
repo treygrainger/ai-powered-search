b90f340253dfeb32c8c663adc43e99efe322fe51
hsm207 <hsm207@users.noreply.github.com>
2024-06-12 04:24:11 +0000

    stub weaviate engine
    Signed-off-by: hsm207 <hsm207@users.noreply.github.com>

---
+++ engines/weaviate/WeaviateEngine.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+from engines.Engine import Engine
+from engines.weaviate.WeaviateCollection import WeaviateCollection
+import weaviate
+
+class WeaviateEngine(Engine):
+    def __init__(self):
+        self.client = weaviate.connect_to_local(port=8090)
+    
+    # @abstractmethod
+    def health_check(self):
+        "Checks the state of the search engine returning a boolean"
+        pass
+    
+    # @abstractmethod
+    def print_status(self, response):
+        "Prints the resulting status of a search engine request"
+        pass
+
+    # @abstractmethod
+    def create_collection(self, name):
+        "Create and initialize the schema for a collection, returns the initialized collection"
+        pass
+
+    # @abstractmethod
+    def get_collection(self, name):
+        "Returns initialized object for a given collection"
+        pass
+    
+    # @abstractmethod
+    def apply_schema_for_collection(self, collection):
+        "Applies the appriorate schema for a given collection"
+        pass
+
+    # @abstractmethod
+    def enable_ltr(self, collection):
+        "Initializes LTR dependencies for a given collection"
+        pass
