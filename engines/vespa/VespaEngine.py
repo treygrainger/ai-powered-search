d5ae6c1f7c2eec5bd87d880e6b08c5ef1a44c7a0
Daniel C <dcrouch26@gmail.com>
2026-01-07 09:44:24 -0500

    Vespa engine deployment code

--- engines/vespa/VespaEngine.py
+++ engines/vespa/VespaEngine.py
@@ -1,12 +1,20 @@
+import io
 import json
+import os
+from pathlib import Path
+import time  
+import shutil
+import zipfile
 
 import requests
 
 from engines.Engine import AdvancedFeatures, Engine
-from engines.vespa.config import VESPA_URL
+from engines.vespa.config import VESPA_URL, VESPA_CONFIG_URL
 from engines.vespa.VespaCollection import VespaCollection
 
 STATUS_URL = f"{VESPA_URL}/state/v1/health"
+APP_INFO_URL = f"{VESPA_CONFIG_URL}/application/v2/tenant/default/application/default"
+DEPLOY_URL = f"{VESPA_CONFIG_URL}/application/v2/tenant/default/prepareandactivate"
 
 #https://docs.vespa.ai/en/reference/query-api-reference.html
 #https://docs.vespa.ai/en/writing/document-v1-api-guide.html
@@ -37,8 +45,33 @@ class VespaEngine(Engine):
     
     def print_status(self, response):
         print(json.dumps(response, indent=2))
+      
+    def generate_zip_binary(self, directory="./engines/vespa/build/application/"): 
+        zip_file = shutil.make_archive("any.zip", 'zip', directory)
+        with open(zip_file, mode='rb') as file:
+            fileContent = file.read()
+        os.remove(zip_file)
+        return fileContent 
 
+    def is_application_deployed(self, log=False):
+        try:
+            response = requests.get(APP_INFO_URL)
+            if log: print(response)
+            return response.status_code == 200
+        except:
+            return False
+    
+    def deploy_application(self, force_deploy=True, log=False):
+        if not self.is_application_deployed() or force_deploy:
+            response = requests.post(DEPLOY_URL, headers={"Content-Type": "application/zip"},
+                                     data=self.generate_zip_binary())
+            if log: print(response, response.json())
+            session_id = response.json()["session-id"]
+            response = requests.put(f"{VESPA_CONFIG_URL}/application/v2/tenant/default/session/{session_id}/active")
+            if log: print(response, response.json())
+            
     def create_collection(self, name, log=False):
+        self.deploy_application(log=log)
         return self.get_collection(name)
 
     def get_collection(self, name):
