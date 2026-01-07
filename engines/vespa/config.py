2a7439a2256ee8ed2d8f1fad941c8ee13d9ab42e
Daniel C <dcrouch26@gmail.com>
2026-01-07 09:42:19 -0500

    Adds vespa deployment utilizing deploy api with proper activation Vespa default rankigns to use bm25 Standardize Vespa dense vector hnsw calculation configuration to be consistent with other engines

--- engines/vespa/config.py
+++ engines/vespa/config.py
@@ -2,5 +2,7 @@ import os
 
 VESPA_HOST = os.getenv("AIPS_VESPA_HOST") or "aips-vespa"
 VESPA_PORT = os.getenv("AIPS_VESPA_PORT") or "8080"
+VESPA_CONFIG_PORT = os.getenv("AIPS_VESPA_PORT") or "19071"
 VESPA_URL = f"http://{VESPA_HOST}:{VESPA_PORT}"
+VESPA_CONFIG_URL = f"http://{VESPA_HOST}:{VESPA_CONFIG_PORT}"
 DEFAULT_NAMESPACE = "aips"
